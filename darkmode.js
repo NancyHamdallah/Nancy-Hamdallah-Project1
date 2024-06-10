2cf6a3a4ef37ab5c94c873548dac4ae65a8851fc
Nancy Hamdallah <hamdallah.nancy@gmail.com>
2024-06-10 18:40:28 +0300

    add details and dark mode

---
+++ darkmode.js
@@ -0,0 +1,24 @@
+console.log("DarkMode");
+let darkMode = localStorage.getItem("darkMode");
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+//check if darkmode enabled then turn it off
+// if it's not enabled then turn it on
+const enableDarkMode = () =>{
+    document.body.classList.add("dark-mode");
+    localStorage.setItem('darkMode','enabled');
+};
+const disableDarkMode = () =>{
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem('darkMode',null);
+};
+
+darkModeToggle.addEventListener("click",()=>{
+    if(darkMode !== "enabled"){
+        enableDarkMode();
+    }
+    else{
+        disableDarkMode();
+    }
+
+});
